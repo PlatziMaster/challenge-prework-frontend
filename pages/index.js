7bf2b8ae177078fc725f089696a1f382339556b1
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-08-13 13:52:23 -0500

    Base configurations - Next & TailwindCSS

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Home from 'components/Home';
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Platzi Game</title>
+      </Head>
+      <Home />
+    </>
+  );
+};
+
+export default Index;
---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <main>
+        <h1 className='text-red-700'>Hello world</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
