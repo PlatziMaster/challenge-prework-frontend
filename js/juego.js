b9d71b119e72cb52af758778fa722b0a15e41dac
Cesar Meza <cmeza@mezadigital.com>
2020-08-14 17:29:02 -0500

    Acomodo primario de maquetado pagina inicial

---
+++ js/juego.js
@@ -0,0 +1,21 @@
+
+//Idea de ima barra de estado
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
