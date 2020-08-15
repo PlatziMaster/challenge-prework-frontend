f2935e56eb2e99d87c935540a3ec213444858dca
Luis Hernandez <luisgerarhern@outlook.com>
2020-08-15 16:26:42 -0500

    personajes y funcionalidad agregado

--- PlatziGame/controler/Game.js
+++ PlatziGame/controler/Game.js
@@ -1,2 +1,64 @@
-import Jugadro from '../model/Jugador';
 
+
+const modal = document.getElementById('modalView');
+const modalGif = document.getElementById('modalGif');
+const whinerLabel = document.getElementById('whinerLabel');
+const healtPlayer1 = [
+    document.getElementById('pHealtPlayer1'),
+    document.getElementById('nHealtPlayer1'),
+];
+const healtPlayer2 = [
+    document.getElementById('pHealtPlayer2'),
+    document.getElementById('nHealtPlayer2'),
+];
+const healtPlayers =[ healtPlayer1,healtPlayer2];
+
+let jugadores = [new Jugador(1),new Jugador(2)];
+
+function hacerDañoAJugador(quienLanzoAtaque){
+    const daño = jugadores[quienLanzoAtaque].hacerDaño() ;
+    const quienReciveDano = (quienLanzoAtaque+1)%2;
+    if(!jugadores[quienReciveDano].recibirDaño(daño)){
+        MostrarModal(jugadores[quienLanzoAtaque].getId());
+        return 0;
+    }
+    actualizarBarraDeVida(quienReciveDano);
+   
+}
+function actualizarBarraDeVida(jugador){
+    healtPlayers[jugador][0].style.flexGrow = jugadores[jugador].getHealt()
+    healtPlayers[jugador][1].style.flexGrow = jugadores[jugador].getDamage()
+}
+function MostrarModal(jugador){
+    whinerLabel.innerHTML = `Player ${jugador}`;
+    obtenerGift()
+    .then((gifts)=>{
+        const {data} = gifts;
+        const urslImages = data.map(gif => gif.images.downsized_large.url);
+        let urlElegida = urslImages[Math.floor(Math.random() * urslImages.length)];
+        modalGif.setAttribute('src',urlElegida);
+    })
+    .catch(()=>{
+        console.log('error')
+    })  
+    modal.classList.remove('hide')
+}
+function obtenerGift(){
+    const url = "http://api.giphy.com/v1/gifs/search?q=winner&api_key=4quN3Z1vE3QA3o0qXRor70NioXGu33cN&limit=5";
+    const opts = {crossDomain: true};
+    return new Promise((resolve,reject)=>{
+        $
+		.get(url,opts,data=>{
+			resolve(data)
+		})
+		.fail(() => reject(id)); 
+    });
+}
+function reStartJuego(){
+    modal.classList.add('hide');
+    jugadores = [new Jugador(1),new Jugador(2)];
+    for(let i=0; i<2;i++){
+        healtPlayers[i][0].style.flexGrow = 100;
+        healtPlayers[i][1].style.flexGrow = 0;
+    }
+}
--- PlatziGame/model/Jugador.js
+++ PlatziGame/model/Jugador.js
@@ -1,9 +1,18 @@
-export default class Jugador{
+class Jugador{
     constructor(id){
-        this.healt = 100
-        this.id = id
+        this.healt = 100;
+        this.damageRecived = 0;
+        this.id = id;
     }
     hacerDaño = () =>Math.round(Math.random()*30)
-    recibirDaño = (damage) => this.healt -= damage
+    recibirDaño (damage) {
+        this.healt -= damage;
+        this.healt = this.healt<0 ? 0:this.healt;
+        this.damageRecived += damage;
+        return this.healt;
+    }
+    getHealt = ()=> this.healt
+    getDamage = ()=> this.damageRecived
+    getId = ()=>this.id
 
 }
