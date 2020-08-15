a40a1c25d8e5cb40391f10f13829b7fdb2a72752
C3M3Z4 <cmeza@mezadigital.com>
2020-08-15 17:45:24 -0500

    Css de la pantalla principal al 70% y juego al 40

--- js/juego.js
+++ js/juego.js
@@ -1,6 +1,55 @@
+function juego(){
+  var p1 = {vida:100};
+  var p2 = {vida:100};
+  var turno = "a";
+  var max = 30;
+  var min = 1;
+  var ataque = 0;
+
+
+// Comparando si tienen vida o no
+  
+while ((p1.vida > 0) || (p2.vida > 0)){
+    
+    if (turno == "a") {
+      ataque =  Math.floor(Math.random() * (max - min)) + min;
+
+      p2.vida = p2.vida - ataque;
+      console.log(`el jugador 2 recibe ${ataque}`);
+      turno = "b";
+      console.log(`El jugador 2 le quedan ${p2.vida}`)
+    } else {
+      ataque =  Math.floor(Math.random() * (max - min)) + min;
+      p1.vida = p1.vida - ataque;
+     
+      console.log(`el jugador 1 recibe ${ataque}`);
+      turno = "a";
+      console.log(`El jugador 1 le quedan ${p1.vida}`)
+    }
+  }
+  
+
+  if (p1.vida <= 0) {
+    console.log("El jugador 2 ha ganado")
+  } else {
+    console.log("El jugador 1 ha ganado!! ")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Idea de ima barra de estado
-
+/*
 var i = 0;
 function move() {
   if (i == 0) {
@@ -18,4 +67,4 @@ function move() {
       }
     }
   }
-}
+}*/
