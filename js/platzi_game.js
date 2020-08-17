5d3efde5cb62f973047e457d5cfffb64cf49f8d5
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-08-17 02:57:12 -0500

    create a logical prongram

--- js/platzi_game.js
+++ js/platzi_game.js
@@ -1,1 +1,95 @@
-alert('It is connected to platzi_game.js')
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log('Document is ready')
+
+  // Creo elementos HTML
+  const btnPlayerOne = document.getElementById('player_one_btn')
+  const numberPlayerOne = document.getElementById('player_one_number')
+  const barPlayerOne = document.getElementById('player_one_bar')
+  const btnPlayerTwo = document.getElementById('player_two_btn')
+  const numberPlayerTwo = document.getElementById('player_two_number')
+  const barPlayerTwo = document.getElementById('player_two_bar')
+  const againBtn = document.getElementById('again_btn')
+  const modal = document.getElementById('modal')
+
+  class Player {
+    constructor(name, life = 100) {
+      this.name = name
+      this.life = life
+    }
+  }
+
+  function initialize() {
+    modal.classList.add('hidden')
+    if(btnPlayerOne.classList.contains('blocked') ){
+      btnPlayerOne.classList.remove('blocked')
+    }
+    if(!btnPlayerTwo.classList.contains('blocked')) {
+      btnPlayerTwo.classList.add('blocked')
+    }
+    let players = [ new Player('Player 1'), new Player('Player 2')]
+    let turn = 0
+    let play = [ players, turn ]
+    return play
+  }
+
+  function chargePower(max, min){
+    let digito_aleatorio = Math.round((max - min) * Math.random()) + min;
+    return digito_aleatorio;
+  }
+
+  function losingHealth(powerAttack){
+    console.log('losingHealth')
+    if(turn === 0) {
+      players[1].life = players[1].life - powerAttack
+    }else {
+      players[0].life = players[0].life - powerAttack
+    }
+  }
+
+  function toggleBtn(btnToRemove, btnToAdd) {
+      btnToRemove.classList.remove('blocked')
+      btnToAdd.classList.add('blocked')
+  }
+
+  function changeTurn() { 
+    if (turn === 0) {
+      turn = 1 
+      toggleBtn(btnPlayerTwo, btnPlayerOne)
+    }else {
+      turn = 0 
+      toggleBtn(btnPlayerOne,btnPlayerTwo)
+    }
+  }
+
+  function youWin() {
+    console.log('youWin')
+    if(players[turn].life <= 0) {
+      changeTurn()
+      console.log(`The winner is: ${players[turn].name}`)
+      modal.classList.remove('hidden')
+    }
+  }
+
+  function playerAttack() {
+    console.log('playerAttack')
+    let powerAttack = chargePower(90, 10)
+    console.log(powerAttack)
+    losingHealth(powerAttack)
+    console.log(players)
+    changeTurn()
+    console.log(turn)
+    youWin()
+  }
+
+  function restart() {
+    window.location.reload()
+  }
+  
+  let play = initialize();
+  let players = play[0]
+  let turn = play[1]
+  console.log("comenzo el juego.")
+  btnPlayerOne.addEventListener('click', playerAttack )
+  btnPlayerTwo.addEventListener('click', playerAttack )
+  againBtn.addEventListener('click', restart)
+});
