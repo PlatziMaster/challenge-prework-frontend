a13858d2ee0339829ee9ce09d625c627e79522e7
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-08-17 11:56:41 -0500

    homepage y modal

---
+++ js/PlatziGame.js
@@ -0,0 +1,21 @@
+const boton = getElementById("Button");
+boton.addEventListener("click",attack );
+
+    const round = 1 ;
+    const a =( Math.round(Math.ramdon() * (MAX_POWER-MIN_POWER) + MIN_POWER);
+    while (vidaPlayer_1 > 0 && vidaPlayer_2 ){
+        if round = 1 {
+            vidaPlayer_2= (vidaPlayer_2 - a );
+            round = 0 ;
+            console.log("La vida del jugador 2 es: " + vidaPlayer_2);
+        else
+        vidaPlayer_1 = (vidaPlayer_1 - a );
+        console.log("la vida del jugador 1 es:" + vidaPlayer_2)
+        }
+    }
+
+const vidaPlayer_1 = 500
+const vidaPlayer_2 = 500
+const MIN_POWER = 10;
+const MAX_POWER = 50;
+const turno = 1 
