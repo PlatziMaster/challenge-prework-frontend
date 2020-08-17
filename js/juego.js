561f3e0b7bf65ab29e345b3ac1e61c81f84154de
C3M3Z4 <cmeza@mezadigital.com>
2020-08-16 22:47:37 -0500

    Pruebas con juego en JS. pendientes personajes en CSS

--- js/juego.js
+++ js/juego.js
@@ -1,45 +1,86 @@
-function juego(){
-  var p1 = {vida:100};
-  var p2 = {vida:100};
-  var turno = "a";
-  var max = 30;
-  var min = 1;
-  var ataque = 0;
+var vidap1 = 100;
+var vidap2 = 100;
+var turno = 1;
+var max = 10;
+var min = 1;
+var ataque = 0;
 
 
-  // p1 = document.getElementById('#payer1-healt_avg-txt');
-  // p1.innerHTML = p1.vida;
 
-  $('#payer1-healt_avg-txt').text(p1.vida)
+        while ((vidap1 > 0) || (vidap2 > 0)){
+            
+          if (turno == 1) {
+
+                ataque =  Math.floor(Math.random() * (max - min)) + min;
+
+                vidap2 = vidap2 - ataque;
+                console.log(`el jugador 2 recibe ${ataque}`);
+                turno = 2;
+                console.log(`El jugador 2 le quedan ${vidap2}`);
+                
+           
+          } else {
+            
+              ataque =  Math.floor(Math.random() * (max - min)) + min;
+              vidap1 = vidap1 - ataque;
+            
+              console.log(`el jugador 1 recibe ${ataque}`);
+              turno = 1;
+              console.log(`El jugador 1 le quedan ${vidap1}`);
+              
+
+          }
+
+        }
+
+        if (vidap1 <= 0) {
+          console.log("El jugador 2 ha ganado");
+          
+        } else {
+          console.log("El jugador 1 ha ganado!! ")
+        }
+
+    
+        $(document).ready(function(){
+
+          $("#player1-healt_avg-txt").text(vidap1);
+          $("#player2-healt_avg-txt").text(vidap2);
+          $("#btn-player1").click(function(){ $("#player2-healt_avg-txt").text(vidap2);});
+          $("#btn-player2").click(function(){$("#player1-healt_avg-txt").text(vidap1);});
+        });
+
+
+
 
 // Comparando si tienen vida o no
   
-while ((p1.vida > 0) || (p2.vida > 0)){
+// while ((p1.vida > 0) || (p2.vida > 0)){
     
-    if (turno == "a") {
-      ataque =  Math.floor(Math.random() * (max - min)) + min;
+//     if (turno == "a") {
+//       ataque =  Math.floor(Math.random() * (max - min)) + min;
 
-      p2.vida = p2.vida - ataque;
-      console.log(`el jugador 2 recibe ${ataque}`);
-      turno = "b";
-      console.log(`El jugador 2 le quedan ${p2.vida}`)
-    } else {
-      ataque =  Math.floor(Math.random() * (max - min)) + min;
-      p1.vida = p1.vida - ataque;
+//       p2.vida = p2.vida - ataque;
+//       console.log(`el jugador 2 recibe ${ataque}`);
+//       turno = "b";
+//       console.log(`El jugador 2 le quedan ${p2.vida}`)
+//     } else {
+//       ataque =  Math.floor(Math.random() * (max - min)) + min;
+//       p1.vida = p1.vida - ataque;
      
-      console.log(`el jugador 1 recibe ${ataque}`);
-      turno = "a";
-      console.log(`El jugador 1 le quedan ${p1.vida}`)
-    }
-  }
+//       console.log(`el jugador 1 recibe ${ataque}`);
+//       turno = "a";
+//       console.log(`El jugador 1 le quedan ${p1.vida}`)
+//     }
+//   }
   
 
-  if (p1.vida <= 0) {
-    console.log("El jugador 2 ha ganado")
-  } else {
-    console.log("El jugador 1 ha ganado!! ")
-  }
-}
+//   if (p1.vida <= 0) {
+//     console.log("El jugador 2 ha ganado");
+
+//   } else {
+//     console.log("El jugador 1 ha ganado!! ")
+//   }
+
 
 
 
